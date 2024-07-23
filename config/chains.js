127beb18a0ad8264d1d417269dd99b99ab986c4c
AtlasVIA <atlas@vialabs.io>
2024-07-23 20:07:55 +0200

    mainnetz

--- config/chains.js
+++ config/chains.js
@@ -862,7 +862,8 @@ const chainsConfig = {
     "network": "testnet",
     "type": "evm",
     "weth": "0xCdc7D21D3F75809ec8CC3030AB8018A98AD0b296",
-    "feeToken": "0x714853D6197e560013ee161fC259b87E8B3cA7E9"
+    "feeToken": "0x714853D6197e560013ee161fC259b87E8B3cA7E9",
+    "message": "0x714c9202B3B5AF0C0Ad844c2a71803cebBFD3AF5"
   },
   "3441006": { // Manta Pacific Testnet
     "name": "manta-pacific-testnet",
