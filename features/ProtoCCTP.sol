0498585886dc35c1c7828b109b04320f1bfe416b
AtlasVIA <atlas@vialabs.io>
2024-09-11 19:16:50 +0200

    undo stuff

--- features/ProtoCCTP.sol
+++ features/ProtoCCTP.sol
@@ -47,53 +47,23 @@ abstract contract ProtoCCTP is FeatureBase {
     }
 
     /**
-     * Send USDC to destination chain/recipient along with additional arbitrary message data (unless it is a pathway hop)
+     * Send USDC to destination chain/recipient along with additional arbitrary message data.
+     * USDC is sent to the destination _recipient which happens before the MessageV3.process() call.
+     * This means funds will be delivered even if the calling destination is a contract that reverts.
      */
     function _sendUSDC(uint _destChainId, address _recipient, uint _amount, bytes memory _data) internal {
-        // send USDC to the destination chain directly to the _recipient which happens before the MessageV3.process() call
-        // funds will be delivered even if the calling destination is a contract that reverts, as it will on a normal wallet
-        // the same is true on a hop, the funds will be set off for a hop before the process() on the intermediary chain
-        // before any recipient contract is called
-
         IERC20cl(usdc).transferFrom(msg.sender, address(this), _amount);
 
         uint32 _destCircleDomain = feature.circleDomain(_destChainId);
-        if(_destCircleDomain != 99999) {
-            // if the desination chain is a circle chain, send directly
-            IERC20cl(usdc).approve(address(tokenMessenger), _amount);
-            tokenMessenger.depositForBurnWithCaller(
-                _amount, 
-                _destCircleDomain, 
-                addressToBytes32(_recipient), 
-                usdc,
-                addressToBytes32(feature.endpoints(_destChainId))
-            );
-            _sendMessageWithFeature(_destChainId, _data, uint32(9000000), '');
-        } else {
-            uint32 _destPathwayRootCircleDomain = feature.pathwayRootCircleDomain(_destChainId);
-            if(_destPathwayRootCircleDomain != 99999) {
-                // if the destination chain is a pathway chain, send to the root chain
-                if(_destPathwayRootCircleDomain == block.chainid) {
-                    // we are on the root, so lets just send to the final pathway chain
-                    IERC20cl(usdc).approve(address(feature.chainIdToPathwayAddress(_destChainId)), _amount);
-                    IBridgedTokenFiatManager(feature.chainIdToPathwayAddress(_destChainId)).bridge(_recipient, _amount);
-                } else {
-                    // we need to first send to the root, then the pathway chain
-                    IERC20cl(usdc).approve(address(tokenMessenger), _amount);
-                    tokenMessenger.depositForBurnWithCaller(
-                        _amount, 
-                        _destPathwayRootCircleDomain, 
-                        addressToBytes32(feature.endpoints(feature.pathwayRootChainId(_destChainId))), // send to self to be able to forward
-                        usdc,
-                        addressToBytes32(feature.endpoints(feature.pathwayRootChainId(_destChainId)))
-                    );
-                    _data = abi.encode(_recipient, _amount); // override data with final recipient since we are hopping
-                    _sendMessageWithFeature(_destChainId, _data, uint32(9000000), '');
-                }
-            } else {
-                revert("ProtoCCTP: destination chain is not a circle chain or a pathway root chain");
-            }
-        }
+        IERC20cl(usdc).approve(address(tokenMessenger), _amount);
+        tokenMessenger.depositForBurnWithCaller(
+            _amount, 
+            _destCircleDomain, 
+            addressToBytes32(_recipient), 
+            usdc,
+            addressToBytes32(feature.endpoints(_destChainId))
+        );
+        _sendMessageWithFeature(_destChainId, _data, uint32(9000000), '');
     }
 
     function addressToBytes32(address _address) internal pure returns (bytes32) {
