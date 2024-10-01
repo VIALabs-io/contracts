0a5f0751d89b071fd410c189acd606a0d0bc38be
DruuuCLT <121195249+DruuuCLT@users.noreply.github.com>
2024-10-01 19:19:47 +0200

    Update chains.js

--- config/chains.js
+++ config/chains.js
@@ -2071,7 +2071,7 @@ const chainsConfig = {
     "network": "testnet",
     "type": "evm",
     "weth": "0x9d75f706b986F0075b3778a12153390273dE95eC",
-    "feeToken": "0xA1AeC40c1B58226407B8b0dF050c8D9ab0df7a7D", // @note Path to USDC
+    "feeToken": "0xA1AeC40c1B58226407B8b0dF050c8D9ab0df7a7D", // @note Path to USDC, proto CCTP compatible
     "message": "0x3B5b764229b2EdE0162220aF51ab6bf7f8527a4F"
   },
   "43851": { // ZkFair Testnet
