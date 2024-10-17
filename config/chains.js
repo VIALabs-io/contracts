92dd2b1d43b56c5755a97df2de8b870e9e9cf9d1
Druuu <121195249+DruuuCLT@users.noreply.github.com>
2024-10-17 18:55:36 +0200

    Update chains.js

--- config/chains.js
+++ config/chains.js
@@ -8,7 +8,7 @@ const chainsConfig = {
     network: "mainnet",
     type: "evm",
     message: "0x15AC559DA4951c796DB6620fAb286B96840D039A",
-    feeToken: "",
+    feeToken: "0x85DAc390d61cdaCb346701cC4948870A704d3f4a", // @note Path to USDC. Proto CCTP compatible
     weth: "0xCFaEB74409E4C6756C43F75455fc42A6A3FdEb1f"
   },
   "2649": { // Ailayer Mainnet
