15205bfe8002ba5ea962dcce3171ecf2cae2c14b
AtlasVIA <atlas@vialabs.io>
2024-10-30 04:53:17 +0100

    update celo fee token to usdc

--- config/chains.js
+++ config/chains.js
@@ -703,7 +703,7 @@ const chainsConfig = {
     type: "evm",
     message: "0xe3b3274bb685F37C7f17a604039c77a6A16Cfc2a",
 
-    feeToken: "0x37f750B7cC259A2f741AF45294f6a16572CF5cAd",
+    feeToken: "0xcebA9300f2b948710d2653dD7B07f33A8B32118C",
     weth: "0x471EcE3750Da237f93B8E339c536989b8978a438",
     usdc: "0xcebA9300f2b948710d2653dD7B07f33A8B32118C",
     rpc: "https://42220.rpc.vialabs.io/",
